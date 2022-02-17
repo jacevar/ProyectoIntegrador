5e4d23dce194ddfdde32147183f6101341b74b35
jacevar <jac22evar@gmail.com>
2022-02-17 12:49:20 -0500

    guia terminada

--- movie/views.py
+++ movie/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 def home(request):
     #return HttpResponse('<h1>Welcome to Home Page</h1><style>h1{color:blue}</style>')
-    return render(request,'home.html')
+    return render(request,'home.html', {'name':'Jacobo Rave'})
 # Create your views here.

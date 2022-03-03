5a2f2cc6ca80c3ca1306a3a8b51ba377d46785c0
jacevar <jac22evar@gmail.com>
2022-03-03 10:40:36 -0500

    adding images

--- movie/views.py
+++ movie/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Movie
+
 #def home(request):
     #return HttpResponse('<h1>Welcome to Home Page</h1><style>h1{color:blue}</style>')
  #   return render(request,'home.html', {'name':'Jacobo Rave'})
@@ -8,7 +10,8 @@ from django.http import HttpResponse
 
 def home(request):
     searchTerm = request.GET.get('searchMovie')
-    return render(request,'home.html', {'searchTerm':searchTerm})
+    movies = Movie.objects.all()
+    return render(request,'home.html', {'searchTerm':searchTerm,'movies':movies})
 
 
 def about(request):

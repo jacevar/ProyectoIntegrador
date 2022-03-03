227e92c38cb4379f123beb173957409d0569bdf2
jacevar <jac22evar@gmail.com>
2022-03-03 10:01:52 -0500

    formulario de busqueda

--- movie/views.py
+++ movie/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def home(request):
+#def home(request):
     #return HttpResponse('<h1>Welcome to Home Page</h1><style>h1{color:blue}</style>')
-    return render(request,'home.html', {'name':'Jacobo Rave'})
+ #   return render(request,'home.html', {'name':'Jacobo Rave'})
 # Create your views here.
+
+def home(request):
+    searchTerm = request.GET.get('searchMovie')
+    return render(request,'home.html', {'searchTerm':searchTerm})
+
+
+def about(request):
+    return HttpResponse('<h1>Welcome to About page</h1>')
